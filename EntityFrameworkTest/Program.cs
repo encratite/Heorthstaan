cb7ac086a19a4fa0a13bbe81d313330b79f72abb
void <git.email.address@googlemail.com>
2013-10-12 14:24:55 +0200

    Added a LINQ test

--- EntityFrameworkTest/Program.cs
+++ EntityFrameworkTest/Program.cs
@@ -1,13 +1,20 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
 
 namespace EntityFrameworkTest
 {
 	class Sample
 	{
+		[Key]
 		public int Id { get; set; }
 		public int IntegerField { get; set; }
-		public string StringField { get; set; } 
+		public string StringField { get; set; }
 		public bool BooleanField { get; set; }
+
+		public Sample()
+		{ }
 
 		public Sample(int integerField, string stringField, bool booleanField)
 		{
@@ -37,6 +44,12 @@ namespace EntityFrameworkTest
 				Sample sample = new Sample(123, "string", true);
 				context.Samples.Add(sample);
 				context.SaveChanges();
+				var samples = from x in context.Samples
+							  where x.StringField == "string"
+							  select x;
+				Console.WriteLine("Samples:");
+				foreach (var x in samples)
+					Console.WriteLine("Id: {0}, IntegerField: {1}, StringField: {2}, BooleanField: {3}", x.Id, x.IntegerField, x.StringField, x.BooleanField);
 			}
 		}
 	}
