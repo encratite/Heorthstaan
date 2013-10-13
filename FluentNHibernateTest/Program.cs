483dd805a00b53ea87bb6e7adfb987afa67c30d8
void <git.email.address@googlemail.com>
2013-10-13 03:25:09 +0200

    Added a new test for Fluent NHibernate

---
+++ FluentNHibernateTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace FluentNHibernateTest
+{
+	class Sample
+	{
+		public int Id { get; set; }
+		public int IntegerField { get; set; }
+		public string StringField { get; set; }
+		public bool BooleanField { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] arguments)
+		{
+			var configuration = PostgreSQLConfiguration
+				.PostgreSQL82
+				.ConnectionString(x =>
+					x
+					.Host("127.0.0.1")
+					.Username("void")
+					.Database("test"));
+			var sessionFactory = Fluently
+				.Configure()
+				.Database(configuration)
+				.Mappings(x =>
+					x.AutoMappings
+					.Add(AutoMap.AssemblyOf<Sample>()))
+				.BuildSessionFactory();
+
+			using(var session = sessionFactory.OpenSession())
+			{
+				using(var transaction = session.BeginTransaction())
+				{
+					Sample sample = new Sample { IntegerField = 123, StringField = "string", BooleanField = true };
+					session.Save(sample);
+					transaction.Commit();
+				}
+			}
+		}
+	}
+}
